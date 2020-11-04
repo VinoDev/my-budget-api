d504b2d021f44fb894ecfe5b5b10ddac333d2ea4
oleg <vinodevmail@gmail.com>
2020-11-04 16:32:34 +0200

    basic Models added

---
+++ src/my-budget-api/Models/Expense.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_budget_api.Models
+{
+    public class Expense
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Amount { get; set; }
+
+    }
+}
---
+++ src/my-budget-api/Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace my_budget_api
+{
+    public class User
+    {
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
