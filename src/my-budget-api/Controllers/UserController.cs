335cc3dfae3afc066bf173b0153b93f65cc8a9c6
oleg <vinodevmail@gmail.com>
2020-11-11 16:16:16 +0200

    user controller

--- src/my-budget-api/Controllers/UserController.cs
+++ src/my-budget-api/Controllers/UserController.cs
@@ -3,34 +3,98 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using my_budget_api.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace my_budget_api.Controllers
 {
-    [ApiController]
     [Route("[controller]")]
+    [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IDocumentClient _documentClient;
+        readonly string databaseId;
+        readonly string collectionId;
+        public IConfiguration Configuration { get; }
 
-        private readonly ILogger<UserController> _logger;
-
-        public UserController(ILogger<UserController> logger)
+        public UserController(IDocumentClient documentClient, IConfiguration configuration)
         {
-            _logger = logger;
+            _documentClient = documentClient;
+            Configuration = configuration;
+
+            databaseId = Configuration["DatabaseId"];
+            collectionId = "Users";
+
+            BuildCollection().Wait();
+        }
+
+        private async Task BuildCollection()
+        {
+            await _documentClient.CreateDatabaseIfNotExistsAsync(new Database { Id = databaseId });
+            await _documentClient.CreateDocumentCollectionIfNotExistsAsync(
+                UriFactory.CreateDatabaseUri(databaseId),
+                new DocumentCollection { Id = collectionId }
+            );
         }
 
         [HttpGet]
-        public IEnumerable<User> Get()
+        public IQueryable<User> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new User
+            return _documentClient.CreateDocumentQuery<User>(
+                UriFactory.CreateDocumentCollectionUri(databaseId, collectionId),
+                new FeedOptions { MaxItemCount = 10 }
+            );
+        }
+
+        [HttpGet("{id}")]
+        public IQueryable<User> Get(string id)
+        {
+            return _documentClient.CreateDocumentQuery<User>(
+                UriFactory.CreateDocumentCollectionUri(databaseId, collectionId),
+                new FeedOptions { MaxItemCount = 1 }
+            ).Where((user) => user.Id == id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] User user)
+        {
+            var response = await _documentClient.CreateDocumentAsync(
+                UriFactory.CreateDocumentCollectionUri(databaseId, collectionId),
+                user
+            );
+
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(string id, [FromBody] User user)
+        {
+            var response = await _documentClient.ReplaceDocumentAsync(
+                UriFactory.CreateDocumentUri(databaseId, collectionId, id),
+                user
+            );
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            try
             {
-                Id = rng.Next(1, 100),
-                Name = "Nameless",
-                Email = "test@test.com",
-                Password = "pa$$w0rd",
-            })
-            .ToArray();
+                await _documentClient.DeleteDocumentAsync(
+                    UriFactory.CreateDocumentUri(databaseId, collectionId, id)
+                );
+                return Ok();
+
+            }
+            catch
+            {
+                return NoContent();
+            }
         }
     }
 }
--- src/my-budget-api/Models/User.cs
+++ src/my-budget-api/Models/User.cs
@@ -4,10 +4,13 @@ namespace my_budget_api
 {
     public class User
     {
-
-        public long Id { get; set; }
+        [Newtonsoft.Json.JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [Newtonsoft.Json.JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
+        [Newtonsoft.Json.JsonProperty(PropertyName = "email")]
         public string Email { get; set; }
+        [Newtonsoft.Json.JsonProperty(PropertyName = "password")]
         public string Password { get; set; }
 
     }
