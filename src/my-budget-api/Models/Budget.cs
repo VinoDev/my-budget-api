f92d48135a60e8da27b111ebefb0c7a6da5777e8
oleg <vinodevmail@gmail.com>
2020-11-11 16:24:53 +0200

    budget controller

--- src/my-budget-api/Models/Budget.cs
+++ src/my-budget-api/Models/Budget.cs
@@ -7,10 +7,13 @@ namespace my_budget_api.Models
 {
     public class Budget
     {
-        public long Id { get; set; }
+        [Newtonsoft.Json.JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
 
+        [Newtonsoft.Json.JsonProperty(PropertyName = "Name")]
         public string Name { get; set; }
 
+        [Newtonsoft.Json.JsonProperty(PropertyName = "Amount")]
         public int Amount { get; set; }
         
     }
